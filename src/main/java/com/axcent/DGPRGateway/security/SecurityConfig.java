96775e39369ef77eae0f74aa60763e39441f0280
Delikatna <veri.pugliese@gmail.com>
2025-08-04 09:44:49 +0200

    Documentazione gatway

--- src/main/java/com/axcent/DGPRGateway/security/SecurityConfig.java
+++ src/main/java/com/axcent/DGPRGateway/security/SecurityConfig.java
@@ -2,12 +2,14 @@ package com.axcent.DGPRGateway.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-@EnableWebFluxSecurity // o @EnableWebSecurity per MVC
+@EnableWebFluxSecurity
+@EnableMethodSecurity(prePostEnabled = true)  // abilita @PreAuthorize e simili// o @EnableWebSecurity per MVC
 public class SecurityConfig {
 
     @Bean
